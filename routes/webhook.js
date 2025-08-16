4534dcc4712648249332865c12689864900ca42e
sulnoorman <noormansulthan5@gmail.com>
2025-08-16 14:11:40 +0700

    lagi

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,7 +8,7 @@ router.post('/bot', async (req, res) => {
     try {
         res.status(200).send("OK");
         const response = await WebhookController.runBotAppScript();
-        console.log("Deploy success:", stdout);
+        console.log("Deploy success:", response);
     } catch (error) {
         console.error("Deploy failed:", error);
         res.status(error.status || 500).json(error);
