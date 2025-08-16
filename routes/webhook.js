bc5d205a22aac3da82ddbba8a3aa43b754c8a4ed
sulnoorman <noormansulthan5@gmail.com>
2025-08-16 13:44:56 +0700

    fix: time out github webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 router.post('/bot', async (req, res) => {
     try {
+        res.status(200).send("OK");
         const response = await WebhookController.runBotAppScript();
-        res.status(response.status).json(response);
+        console.log("Deploy success:", stdout);
     } catch (error) {
+        console.error("Deploy failed:", err);
         res.status(error.status || 500).json(error);
     }
 });
